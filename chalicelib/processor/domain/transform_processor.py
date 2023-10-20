cefb022bdd3c16b2956a11a6191fe5a82cb0ac98
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-20 23:10:27 +0900

    [feat] done

--- chalicelib/processor/domain/transform_processor.py
+++ chalicelib/processor/domain/transform_processor.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, Mapping, Sequence
 
+from chalicelib.db.model.filter import Filter
 from chalicelib.model.message import Message
 from chalicelib.processor.processor_ifs import ProcessorIfs
 
@@ -10,4 +11,18 @@ class TransformProcessor(ProcessorIfs):
         res = self._repository.upsert(
             rel_name=message.rel_name, db_name=message.db_name, data=data
         )
+        # status 바꾸기
+        invalid_result = self._repository.update_many(
+            rel_name=message.rel_name,
+            db_name=message.db_name,
+            _filter={"status": Filter(op="lt", value=2)},
+            datum={"status": -1},
+        )
+        valid_result = self._repository.update_many(
+            rel_name=message.rel_name,
+            db_name=message.db_name,
+            _filter={"status": Filter(op="eq", value=2)},
+            datum={"status": 1},
+        )
+        self.logger.info(f"invalid: {invalid_result}, valid: {valid_result}")
         return res
